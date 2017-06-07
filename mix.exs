dbc04cff842de90392e1b32da698fda625638757
kodburn <kodburn@gmail.com>
2017-06-07 18:45:40 +0200

    Install HTTPoison(HTTP client) and Poison(JSON parser)

--- mix.exs
+++ mix.exs
@@ -10,24 +10,14 @@ defmodule Issues.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison, :poison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9"},
+      {:poison, "~> 2.2"}
+    ]
   end
 end
